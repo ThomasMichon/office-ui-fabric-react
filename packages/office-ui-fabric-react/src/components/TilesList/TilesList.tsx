beb7d39b8f7a7323dc6820a8bc3999c741ae9f1c
Thomas Michon <tmichon@microsoft.com>
2017-06-20 11:49:47 -0700

    Start TilesList

---
+++ packages/office-ui-fabric-react/src/components/TilesList/TilesList.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+
+export interface ITilesListProps extends React.Props<TilesList> {
+
+}
+
+export interface ITilesListState {
+
+}
+
+export class TilesList extends React.Component<ITilesListProps, ITilesListState> {
+
+}
