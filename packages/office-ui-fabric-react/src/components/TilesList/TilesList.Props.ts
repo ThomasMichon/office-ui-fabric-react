1abca91e54d299c61e885dfc6f02e15bce269f59
Thomas Michon <tmichon@microsoft.com>
2017-06-27 09:19:54 -0700

    Add example page for TilesList component

---
+++ packages/office-ui-fabric-react/src/components/TilesList/TilesList.Props.ts
@@ -0,0 +1,7 @@
+
+import * as React from 'react';
+import { TilesList } from './TilesList';
+
+export interface ITilesListProps extends React.Props<TilesList> {
+
+}
---
+++ packages/office-ui-fabric-react/src/components/TilesList/examples/TilesList.Basic.Example.tsx
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+
+export class TilesListBasicExample extends React.Component<any, any> {
+  public render() {
+    return (
+      <div />
+    );
+  }
+}
