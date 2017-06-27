e870dc80ea444be5d284034afc432a349148537f
Thomas Michon <tmichon@microsoft.com>
2017-06-27 14:20:20 -0700

    Update TilesList example

--- packages/office-ui-fabric-react/src/components/TilesList/examples/TilesList.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/TilesList/examples/TilesList.Basic.Example.tsx
@@ -1,10 +1,39 @@
 
 import * as React from 'react';
+import { TilesList } from '../../TilesList';
+import { autobind } from '../../../Utilities';
+
+const ITEMS = Array.apply(null, { length: 1000 }).map((value: undefined, index: number) => {
+  return {
+    key: `item-${index}`,
+    aspectRatio: 0.5 + 1.5 * Math.random()
+  };
+});
+
+declare class TilesListClass extends TilesList<typeof ITEMS[0]> { }
+
+const TilesListType: typeof TilesListClass = TilesList;
 
 export class TilesListBasicExample extends React.Component<any, any> {
   public render() {
     return (
-      <div />
+      <TilesListType
+        items={ ITEMS }
+        getItemAspectRatio={ this._onGetItemAspectRatio }
+        onRenderCell={ this._onRenderCell }
+      />
+    );
+  }
+
+  @autobind
+  private _onGetItemAspectRatio(item: typeof ITEMS[0]): number {
+    return item && item.aspectRatio || 1;
+  }
+
+  @autobind
+  private _onRenderCell(item: typeof ITEMS[0]) {
+    return (
+      <div>{ item.key }</div>
     );
   }
 }
