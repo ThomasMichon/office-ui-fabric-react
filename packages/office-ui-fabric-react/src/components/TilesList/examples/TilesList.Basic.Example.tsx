a02545e41a933534bba90ded4e65c70fdf4567ae
Thomas Michon <tmichon@microsoft.com>
2017-06-29 09:03:44 -0700

    Add better example aspect ratios for TilesList

--- packages/office-ui-fabric-react/src/components/TilesList/examples/TilesList.Basic.Example.tsx
+++ packages/office-ui-fabric-react/src/components/TilesList/examples/TilesList.Basic.Example.tsx
@@ -3,10 +3,29 @@ import * as React from 'react';
 import { TilesList } from '../../TilesList';
 import { autobind } from '../../../Utilities';
 
+type IAspectRatioByProbability = { [probability: string]: number; };
+
+const PROBABILITIES: IAspectRatioByProbability = {
+  '0.95': 3,
+  '0.90': 1 / 3,
+  '0.80': 16 / 9,
+  '0.70': 9 / 16,
+  '0.40': 4 / 3,
+  '0.10': 3 / 4,
+  '0.00': 1
+};
+
+const ENTRIES = Object.keys(PROBABILITIES).map((key: keyof IAspectRatioByProbability) => ({
+  probability: Number(key),
+  aspectRatio: PROBABILITIES[key]
+}));
+
 const ITEMS = Array.apply(null, { length: 1000 }).map((value: undefined, index: number) => {
+  const seed = Math.random();
+
   return {
     key: `item-${index}`,
-    aspectRatio: 0.5 + 1.5 * Math.random()
+    aspectRatio: ENTRIES.filter((entry: { probability: number; aspectRatio: number; }) => seed > entry.probability)[0].aspectRatio
   };
 });
 
@@ -17,11 +36,15 @@ const TilesListType: typeof TilesListClass = TilesList;
 export class TilesListBasicExample extends React.Component<any, any> {
   public render() {
     return (
-      <TilesListType
-        items={ ITEMS }
-        getItemAspectRatio={ this._onGetItemAspectRatio }
-        onRenderCell={ this._onRenderCell }
-      />
+      <div>
+        <div>Start</div>
+        <TilesListType
+          items={ ITEMS }
+          getItemAspectRatio={ this._onGetItemAspectRatio }
+          onRenderCell={ this._onRenderCell }
+        />
+        <div>End</div>
+      </div>
     );
   }
 
